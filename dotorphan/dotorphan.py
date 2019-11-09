eb63c99af6b1a7b4267fd53f290fab8306319b3d
umaumax <umauma7083@gmail.com>
2019-11-09 20:43:07 +0900

    Fromat

--- dotorphan/dotorphan.py
+++ dotorphan/dotorphan.py
@@ -11,7 +11,8 @@ import networkx
 
 def demangle(input_file_object, output_file_object, verbose=True):
     args = ['c++filt', '-n']
-    pipe = subprocess.Popen(args, stdin=input_file_object, stdout=output_file_object)
+    pipe = subprocess.Popen(args, stdin=input_file_object,
+                            stdout=output_file_object)
     _, errs = pipe.communicate()
     if errs:
         print("[log] c++filt demangle failed: {}".format(errs), file=sys.stderr)
@@ -36,7 +37,8 @@ def remove_nodes(graph, node_names, verbose=True):
             graph.remove_node(node_name)
         else:
             if verbose:
-                print("[remove_nodes] not found node name '{}'".format(node_name), file=sys.stderr)
+                print("[remove_nodes] not found node name '{}'".format(
+                    node_name), file=sys.stderr)
             return False
     return True
 
@@ -45,18 +47,22 @@ def remove_traversed_nodes_regex(graph, node_patterns, verbose=True):
     for node_pattern in node_patterns:
         matcher = re.compile(node_pattern)
         for node in list([n for n in graph.nodes() if matcher.search(n)]):
-            print("[remove_traversed_nodes_regex] match {} by '{}' regex pattern".format(node, node_pattern), file=sys.stderr)
-            graph.remove_nodes_from(list(set([element for tuple in list(networkx.edge_bfs(graph, [str(node)])) for element in tuple])))
+            print("[remove_traversed_nodes_regex] match {} by '{}' regex pattern".format(
+                node, node_pattern), file=sys.stderr)
+            graph.remove_nodes_from(list(set([element for tuple in list(
+                networkx.edge_bfs(graph, [str(node)])) for element in tuple])))
     return True
 
 
 def remove_traversed_nodes(graph, node_names, verbose=True):
     for node_name in node_names:
         if graph.has_node(node_name):
-            graph.remove_nodes_from(list(set([element for tuple in list(networkx.edge_bfs(graph, [node_name])) for element in tuple])))
+            graph.remove_nodes_from(list(set([element for tuple in list(
+                networkx.edge_bfs(graph, [node_name])) for element in tuple])))
         else:
             if verbose:
-                print("[remove_traversed_nodes] not found node name '{}'".format(node_name), file=sys.stderr)
+                print("[remove_traversed_nodes] not found node name '{}'".format(
+                    node_name), file=sys.stderr)
             return False
     return True
 
@@ -66,9 +72,11 @@ def run(input, output, log_output, args):
     # NOTE: input type: filename or file object
     G = networkx.DiGraph(networkx.drawing.nx_pydot.read_dot(input))
     if args.relabel:
-        G = networkx.relabel_nodes(G, lambda x: G.node[x]['label'] if 'label' in G.node[x] else x)
+        G = networkx.relabel_nodes(
+            G, lambda x: G.node[x]['label'] if 'label' in G.node[x] else x)
 
-    ret = remove_traversed_nodes_regex(G, args.remove_traversed) if args.regex else remove_traversed_nodes(G, args.remove_traversed)
+    ret = remove_traversed_nodes_regex(
+        G, args.remove_traversed) if args.regex else remove_traversed_nodes(G, args.remove_traversed)
     if not ret:
         return False
     ret = remove_nodes(G, args.remove)
@@ -87,7 +95,8 @@ def run(input, output, log_output, args):
 
     # NOTE: get root nodes
     log_output.write('# orphan root nodes' + '\n')
-    log_output.write(str([n for n, d in filtered_graph.in_degree() if d == 0]) + '\n')
+    log_output.write(
+        str([n for n, d in filtered_graph.in_degree() if d == 0]) + '\n')
 
     cnt = 0
     log_output.write('# orphan edges from root node' + '\n')
@@ -95,9 +104,11 @@ def run(input, output, log_output, args):
         # NOTE: root node color setting
         filtered_graph.node[node]['fillcolor'] = 'darkorange'
         filtered_graph.node[node]['style'] = 'filled'
-        duplicated_nodes = list([element for tupl in list(networkx.edge_bfs(filtered_graph, [node])) for element in tupl])
+        duplicated_nodes = list([element for tupl in list(
+            networkx.edge_bfs(filtered_graph, [node])) for element in tupl])
         unique_nodes = [node]  # for orphan root node (without edges)
-        [unique_nodes.append(item) for item in duplicated_nodes if item not in unique_nodes]
+        [unique_nodes.append(item)
+         for item in duplicated_nodes if item not in unique_nodes]
         log_output.write('[{}]{}\n'.format(cnt, str(unique_nodes)))
         cnt += 1
 
@@ -108,23 +119,34 @@ def run(input, output, log_output, args):
     if args.gui:
         pygraphviz_module_assert()
         if not networkx.is_empty(filtered_graph):
-            networkx.nx_agraph.view_pygraphviz(filtered_graph, prog=args.agrpah_prog)
+            networkx.nx_agraph.view_pygraphviz(
+                filtered_graph, prog=args.agrpah_prog)
         else:
             print("# [log] filtered graph is empty", file=sys.stderr)
     return True
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--demangle', action='store_false', help='demangle c++ symbol')
-    parser.add_argument('--relabel', action='store_false', help='relabel node name by label attr')
-    parser.add_argument('--regex', action='store_true', help='enable regex of --remove-traversed')
-    parser.add_argument('--gui', action='store_true', help='show graph (requires: pygraphviz)')
-    parser.add_argument('--agrpah-prog', default='dot', type=str, help='graph drawing prog (neato, dot, twopi, circo, fdp, nop, wc, acyclic, gvpr, gvcolor, ccomps, sccmap, tred, sfdp, unflatten)')
-    parser.add_argument('--remove', default=['"{external node}"'], type=str, nargs='*', help='remove node names')
-    parser.add_argument('--remove-traversed', default=['"{main}"'], type=str, nargs='*', help='remove nodes traversed from these node names')
-    parser.add_argument('-o', '--output', default='', type=str, help='output filepath (dot, svg, png, pdf, ...)')
-    parser.add_argument('--log-output', default='/dev/stdout', type=str, help='log output filepath')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--demangle', action='store_false',
+                        help='demangle c++ symbol')
+    parser.add_argument('--relabel', action='store_false',
+                        help='relabel node name by label attr')
+    parser.add_argument('--regex', action='store_true',
+                        help='enable regex of --remove-traversed')
+    parser.add_argument('--gui', action='store_true',
+                        help='show graph (requires: pygraphviz)')
+    parser.add_argument('--agrpah-prog', default='dot', type=str,
+                        help='graph drawing prog (neato, dot, twopi, circo, fdp, nop, wc, acyclic, gvpr, gvcolor, ccomps, sccmap, tred, sfdp, unflatten)')
+    parser.add_argument(
+        '--remove', default=['"{external node}"'], type=str, nargs='*', help='remove node names')
+    parser.add_argument('--remove-traversed', default=[
+                        '"{main}"'], type=str, nargs='*', help='remove nodes traversed from these node names')
+    parser.add_argument('-o', '--output', default='', type=str,
+                        help='output filepath (dot, svg, png, pdf, ...)')
+    parser.add_argument('--log-output', default='/dev/stdout',
+                        type=str, help='log output filepath')
     parser.add_argument('input', type=str, help='input dot file')
 
     args, extra_args = parser.parse_known_args()
