7723cb90715ca3ee94c1b6a91ae1e6cd64e94a29
umaumax <umauma7083@gmail.com>
2019-11-09 17:07:14 +0900

    Fix bug and refactor

--- dotorphan/dotorphan.py
+++ dotorphan/dotorphan.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import subprocess
 import sys
+import tempfile
 
 import argparse
 import networkx
@@ -15,43 +17,126 @@ def pygraphviz_module_assert():
         sys.exit(1)
 
 
+def remove_nodes(graph, node_names, verbose=True):
+    # NOTE: simple method without warning
+    # graph.remove_nodes_from(node_names)
+    for node_name in node_names:
+        if graph.has_node(node_name):
+            graph.remove_node(node_name)
+        else:
+            if verbose:
+                print("[remove_nodes] not found node name '{}'".format(node_name), file=sys.stderr)
+            return False
+    return True
+
+
+def remove_traversed_nodes(graph, node_names, verbose=True):
+    for node_name in node_names:
+        if graph.has_node(node_name):
+            graph.remove_nodes_from(list(set([element for tuple in list(networkx.edge_bfs(graph, [node_name])) for element in tuple])))
+        else:
+            if verbose:
+                print("[remove_traversed_nodes] not found node name '{}'".format(node_name), file=sys.stderr)
+            return False
+    return True
+
+
+def run(input, output, log_output, args):
+    filtered_graph = networkx.DiGraph()
+    # NOTE: input type: filename or file object
+    G = networkx.DiGraph(networkx.drawing.nx_pydot.read_dot(input))
+    if args.relabel:
+        G = networkx.relabel_nodes(G, lambda x: G.node[x]['label'] if 'label' in G.node[x] else x)
+
+    ret = remove_traversed_nodes(G, args.remove_traversed)
+    if not ret:
+        return False
+    ret = remove_nodes(G, args.remove)
+    if not ret:
+        return False
+
+    log_output.write('# orphan nodes' + '\n')
+    for name in list([k for k, v in G.degree() if v == 0]):
+        log_output.write(name)
+        filtered_graph.add_node(G.node[name])
+
+    log_output.write('# orphan edges' + '\n')
+    for edges in list([G.edges(component) for component in networkx.connected_components(G.to_undirected()) if len(G.edges(component)) > 0]):
+        # display_names = list(map(lambda x: (G.node[x[0]]['label'] if 'label' in G.node[x[0]] else x[0], G.node[x[1]]['label'] if 'label' in G.node[x[1]] else x[1]), list(edges)))
+        log_output.write(str(edges) + '\n')
+        filtered_graph.add_edges_from(edges)
+
+    # NOTE: get root nodes
+    log_output.write('# orphan root nodes' + '\n')
+    log_output.write(str([n for n, d in filtered_graph.in_degree() if d == 0]) + '\n')
+
+    cnt = 0
+    log_output.write('# orphan edges from root node' + '\n')
+    for node in [n for n, d in filtered_graph.in_degree() if d == 0]:
+        # NOTE: root node color setting
+        filtered_graph.node[node]['fillcolor'] = 'darkorange'
+        filtered_graph.node[node]['style'] = 'filled'
+        duplicated_nodes = list([element for tupl in list(networkx.edge_bfs(filtered_graph, [node])) for element in tupl])
+        unique_nodes = [node]  # for orphan root node (without edges)
+        [unique_nodes.append(item) for item in duplicated_nodes if item not in unique_nodes]
+        log_output.write('[{}]{}\n'.format(cnt, str(unique_nodes)))
+        cnt += 1
+
+    if output:
+        pygraphviz_module_assert()
+        agrpah = networkx.nx_agraph.to_agraph(filtered_graph)
+        agrpah.draw(output, prog=args.agrpah_prog)
+    if args.gui:
+        pygraphviz_module_assert()
+        if not networkx.is_empty(filtered_graph):
+            networkx.nx_agraph.view_pygraphviz(filtered_graph, prog=args.agrpah_prog)
+        else:
+            print("# [log] filtered graph is empty", file=sys.stderr)
+    return True
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--demangle', action='store_false', help='demangle c++ symbol')
+    parser.add_argument('--relabel', action='store_false', help='relabel node name by label attr')
     parser.add_argument('--gui', action='store_true', help='show graph (requires: pygraphviz)')
     parser.add_argument('--agrpah-prog', default='dot', type=str, help='graph drawing prog (neato, dot, twopi, circo, fdp, nop, wc, acyclic, gvpr, gvcolor, ccomps, sccmap, tred, sfdp, unflatten)')
-    parser.add_argument('--exclude', default=['main'], type=str, nargs='*', help='exclude node names')
+    parser.add_argument('--remove', default=['"{external node}"'], type=str, nargs='*', help='remove node names')
+    parser.add_argument('--remove-traversed', default=['"{main}"'], type=str, nargs='*', help='remove nodes traversed from these node names')
     parser.add_argument('-o', '--output', default='', type=str, help='output filepath (dot, svg, png, pdf, ...)')
+    parser.add_argument('--log-output', default='/dev/stdout', type=str, help='log output filepath')
     parser.add_argument('input', type=str, help='input dot file')
 
     args, extra_args = parser.parse_known_args()
-
-    G = networkx.DiGraph(networkx.drawing.nx_pydot.read_dot(args.input))
-
-    filtered_graph = networkx.DiGraph()
-
     if len(extra_args) > 0:
         print("found extra args {}".format(extra_args), file=sys.stderr)
         parser.print_help()
         sys.exit(1)
-    exclude_nodes = args.exclude
-    print(exclude_nodes)
-    print('# all orphan nodes')
-    for node in list([k for k, v in G.degree() if v == 0 and k not in exclude_nodes]):
-        print(node)
-        filtered_graph.add_node(node)
 
-    print('# all orphan edges')
-    for edges in list([G.edges(component) for component in networkx.connected_components(G.to_undirected()) if len(G.edges(component)) > 0 and len(set(exclude_nodes) & set(component)) == 0]):
-        print(edges)
-        filtered_graph.add_edges_from(edges)
+    ret = True
+    with open(args.log_output, 'w') as log_file:
+        if not args.demangle:
+            ret = run(args.input, args.output, log_file, args)
+        else:
+            with open(args.input, 'r') as input_file:
+                with tempfile.TemporaryFile(mode='w+') as tmp_output_file:
+                    ret = demangle(input_file, tmp_output_file)
+                    if ret:
+                        tmp_output_file.seek(0)
+                        ret = run(tmp_output_file, args.output, log_file, args)
+    if not ret:
+        sys.exit(1)
+    return True
 
-    if args.gui:
-        pygraphviz_module_assert()
-        networkx.nx_agraph.view_pygraphviz(G, prog=args.agrpah_prog)
-    if args.output:
-        pygraphviz_module_assert()
-        agrpah = networkx.nx_agraph.to_agraph(filtered_graph)
-        agrpah.draw(args.output, prog=args.agrpah_prog)
+
+def demangle(input_file_object, output_file_object, verbose=True):
+    args = ['c++filt', '-n']
+    pipe = subprocess.Popen(args, stdin=input_file_object, stdout=output_file_object)
+    _, errs = pipe.communicate()
+    if errs:
+        print("[log] c++filt demangle failed: {}".format(errs), file=sys.stderr)
+        return False
+    return True
 
 
 if __name__ == '__main__':
